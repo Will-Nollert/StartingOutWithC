d7fb0a49f6086fbe61c188ae92aea9bcf9e59ff8
William Nollert <will.nollert@gmail.com>
2023-02-08 20:21:31 -0600

    homework comments

--- chapter_5/studentLineUp/nollert_Hmwk5.cpp
+++ chapter_5/studentLineUp/nollert_Hmwk5.cpp
@@ -31,28 +31,32 @@ int main()
         return 0;
     }
 
+    //give first and last a value that we know is some name X on a list of names 
     inputFile >> singleStudentName;
-    first = singleStudentName;
-    last = singleStudentName;
+    first = last = singleStudentName;
 
+    //for the user input number of times do: 
     for (int i = 0; i < numberOfStudents; i++)
     {
-        inputFile >> singleStudentName; // Assinges the comparison value name to singleStuent name
+        inputFile >> singleStudentName; // Assinges the comparison value name to singleStuent name appart from first and last 
 
         // After thinking about it even more I realized you dont kneed to check every char
         // you can just compare the whole vars weight vs each other.
 
+        //first get assinged to the first read name and then compared in weight to next
+        //eventully the "A"ist will will out on top
         if (first > singleStudentName)
         {
             first = singleStudentName;
         }
-
+        //same logic but with dif opperator 
         if (last < singleStudentName)
         {
             last = singleStudentName;
         }
     }
 
+    //print result
     cout << endl;
     cout << "Student Order" << endl;
     cout << "---------------------------------\n";
