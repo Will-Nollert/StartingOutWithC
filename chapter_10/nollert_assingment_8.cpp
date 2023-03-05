12c1cddd701753e3a534aa58c3ec8f13c9a3e0d6
William Nollert <will.nollert@gmail.com>
2023-03-05 09:32:37 -0600

    feat: addtional cases

--- chapter_10/nollert_assingment_8.cpp
+++ chapter_10/nollert_assingment_8.cpp
@@ -7,15 +7,17 @@ int main()
 {
 	const int LENGTH = 101;
 	char List[LENGTH];
-	int Upper, Lower, Digit;
+	int Upper, Lower, Digit, Punc;
 
 	// Ask user to create a password
 	cout << "Create your password\n"
 		 << "passwords must meet the following criteria:\n"
-		 << " • The password should be at least six characters long.\n"
+		 << " • The password should be at least 15 characters long.\n"
 		 << " • The password should contain at least one uppercase\n"
 		 << "   and at least one lowercase letter.\n"
-		 << " • The password should have at least one digit.\n";
+		 << " • The password should have at least one digit.\n"
+		 << " • And, password must contain at least one special character.\n";
+
 
 	do
 	{		
@@ -28,16 +30,20 @@ int main()
 		{
 			if (isupper(List[i]))
 				Upper++;
+			if (ispunct(List[i]))
+				Punc++;
 			if (islower(List[i]))
 				Lower++;
 			if(isdigit(List[i]))
 				Digit++;
 		}
 	
-		if (strlen(List) < 6)
-			cout << "password is not at least six characters long.\n";
+		if (strlen(List) < 15)
+			cout << "password is not at least 15 characters long.\n";
 		if (Upper == 0)
 			cout << "password does not contain at least one uppercase letter.\n";
+		if (Punc == 0)
+			cout << "password does not contain at least one special character.\n";
 		if (Lower == 0)
 			cout << "password does not contain at least one lowercase letter.\n";
 		if (Digit == 0)
